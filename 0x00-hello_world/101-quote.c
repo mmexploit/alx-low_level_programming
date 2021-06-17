92ab100d1f6ce0e1d690b203d4b27614eb4f94b3
Mubarek Endrie <mmexploit1@gmail.com>
2021-06-17 10:37:10 -0700

    101

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - Main part of the program to print text.
+ * Return: will return 1 if successfully completed.
+ */
+int main(void)
+{
+write(2, "and that piece of art is useful - Dora Korpar, 2015-10-19\n", 58);
+return (1);
+}
