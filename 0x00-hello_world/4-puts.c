03a7e512199b98a7d72e05755e3e664bbf944564
Mubarek Endrie <mmexploit1@gmail.com>
2021-06-17 02:33:22 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 /**
- *main-This is the main function
- *return-This returns with  a 0 
- */
+*main-This is the main function
+*return-This returns with  a 0 
+*/
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");
