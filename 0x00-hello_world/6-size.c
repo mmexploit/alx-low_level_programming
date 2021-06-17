503a3408f129b91be9b96a7c6b22746113dcc96f
Mubarek Endrie <mmexploit1@gmail.com>
2021-06-17 09:04:05 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,9 +6,9 @@
 int main(void)
 {
 printf("Size of a char: %zu %s", sizeof(char), "byte(s)\n");
-printf("Size of an int: %zu %s", sizeof(int), "byte(s)");
-printf("Size of a long int: %zu %s", sizeof(long), "byte(s)");
-printf("Size of a long long int: %zu %s", sizeof(long long), "byte(s)");
-printf("Size of a float: %zu %s", sizeof(float), "byte(s)");
+printf("Size of an int: %zu %s", sizeof(int), "byte(s)\n");
+printf("Size of a long int: %zu %s", sizeof(long), "byte(s)\n");
+printf("Size of a long long int: %zu %s", sizeof(long long), "byte(s)\n");
+printf("Size of a float: %zu %s", sizeof(float), "byte(s)\n");
 return (0);
 }
