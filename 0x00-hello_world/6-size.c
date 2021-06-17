fa9a6c571e6251482f3943db1dc67b02e0d848aa
Mubarek Endrie <mmexploit1@gmail.com>
2021-06-17 04:06:17 -0700

    6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,6 +6,7 @@
 int main(void)
 {
 printf("Size of a char: %zu %s", sizeof(char), "byte(s)");
-
+printf("Size of an int: %zu %s", sizeof(int), "byte(s)");
+printf("Size of a long int: %zu %s", sizeof(char), "byte(s)");
 return (0);
 }
