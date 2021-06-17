05d17de05771e61ca33ec9207137d0b75440652f
Mubarek Endrie <mmexploit1@gmail.com>
2021-06-17 02:52:42 -0700

    5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ *main-This is the main function
+ *Return:This returns with  a 0
+*/
+int main(void)
+{
+puts("with proper grammar, but the outcome is a piece of art,");
+return (0);
+}
