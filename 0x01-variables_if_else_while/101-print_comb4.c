b772549b53bc71898089ee968ba080c89a131bcd
mmexploit <67064862+mmexploit@users.noreply.github.com>
2021-07-11 12:11:33 +0100

    Create 101-print_comb4.c

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - print numbers from 00 to 99.
+(*
+ * Return: print 0 on success
+ */
+int main(void)
+    int i;
+    int j;
+    int k;
+    for (i='0';i<='9';i++){
+        for(j='0';j<='9';j++){
+            for(k='0';k<='9';k++){
+                if(i<j && j<k){
+                    putchar(i);
+                    putchar(j);
+                    putchar(k);
+                    putchar(',');
+                    putchar(' ');
+                }
+            }
+        }
+    }
+    return (0);
+}
