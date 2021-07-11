d6a84d44813535182493fc5c6d9e289d8bd7dbda
mmexploit <67064862+mmexploit@users.noreply.github.com>
2021-07-11 13:35:45 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,25 +9,26 @@ int main(void)
 int i;
 int j;
 int k;
-    for (i = '0'; i <= '9'; i++)
+for (i = '0'; i <= '9'; i++)
+{
+    for(j = '0'; j <= '9'; j++)
     {
-        for(j = '0'; j <= '9'; j++)
+        for(k = '0'; k <= '9'; k++)
         {
-            for(k = '0'; k <= '9'; k++)
+            if(i < j && j < k)
             {
-                if(i < j && j < k)
+                putchar(i);
+                putchar(j);
+                putchar(k);
+                if(i != '7' || j != '8' || k != '9')
                 {
-                    putchar(i);
-                    putchar(j);
-                    putchar(k);
-                    if(i != '7' || j != '8' || k != '9'){
                     putchar(',');
                     putchar(' ');
-                    }
                 }
             }
         }
     }
-    putchar('\n');
-    return (0);
+}
+putchar('\n');
+return (0);
 }
