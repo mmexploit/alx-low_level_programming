a6ab467b7430142c344d845a2d692a583767f644
mmexploit <67064862+mmexploit@users.noreply.github.com>
2021-07-11 12:43:07 +0100

    Update 101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -15,9 +15,12 @@ int main(void){
                     putchar(i);
                     putchar(j);
                     putchar(k);
+                    if(i!=7 && j!=8 && k!=9){
                     putchar(',');
                     putchar(' ');
-                }
+                    }
+                
+                    }
             }
         }
     }
